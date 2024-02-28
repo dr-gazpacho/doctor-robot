3d596aabb6e26398f04559841cf954629155624b
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2024-02-27 21:56:31 -0600

    sav mor

--- dr-robo.c
+++ dr-robo.c
@@ -44,13 +44,7 @@ void do_linear_brightness(bool enabled, uint slice_num, uint chan){
 int main() {
 	
 	stdio_init_all();
-	gpio_init(2);
-	gpio_init(3);
-	gpio_set_function(4, GPIO_FUNC_PWM);
-	gpio_set_function(2, GPIO_FUNC_SIO);
-	gpio_set_function(3, GPIO_FUNC_SIO);
-	gpio_set_dir(2, true);
-	gpio_set_dir(3, true);
+	gpio_set_function(16, GPIO_FUNC_PWM);
 	gpio_set_function(21, GPIO_FUNC_PWM);
 	gpio_set_function(22, GPIO_FUNC_PWM);
 	
@@ -63,8 +57,8 @@ int main() {
 	uint chan22 = pwm_gpio_to_channel(22);
 	
 	//light
-	uint led_slice_num = pwm_gpio_to_slice_num(4);
-	uint chan4 = pwm_gpio_to_channel(4);
+	uint led_slice_num = pwm_gpio_to_slice_num(16);
+	uint chan16 = pwm_gpio_to_channel(16);
 	
 	pwm_set_freq_duty(arm_slice, chan21, 50, 0);
 	pwm_set_enabled(arm_slice, true);
@@ -72,11 +66,12 @@ int main() {
 	pwm_set_freq_duty(slice_num, chan22, 50, 0);
 	pwm_set_enabled(slice_num, true);
 	
-	pwm_set_freq_duty(led_slice_num, chan4, 2000, 0);
+	pwm_set_freq_duty(led_slice_num, chan16, 2000, 0);
 	pwm_set_enabled(led_slice_num, true);
 	
 	
 	//initialize the pins for the buttons
+	//button for LEDs
 	gpio_set_function(12, GPIO_FUNC_SIO);
 	//buttons for servos
 	gpio_set_function(10, GPIO_FUNC_SIO);
@@ -109,8 +104,6 @@ int main() {
 	int armHeadToggleState = 0;
 	int smoothMoveToggleState = 0;
 	int led = 0;
-	int eyeRight = 2;
-	int eyeLeft = 3;
 	int increase = 0;
 	int decrease = 0;
 	int reset = 0;
@@ -120,8 +113,6 @@ int main() {
 	pwm_set_duty(slice_num, chan22, count);
 	
 	while(1){
-		gpio_put(eyeRight, 1);
-		gpio_put(eyeLeft, 1);
 		toggleSmoothMove = !gpio_get(10);
 		toggleArmHeadMode = !gpio_get(11);
 		led = !gpio_get(12);
@@ -173,7 +164,6 @@ int main() {
 				}
 				//handle decrease in all modes
 				if(decrease) {
-					printf("decrease");
 					state = 1;
 					if(armHeadToggleState && !smoothMoveToggleState) {
 						if(armCount > 2){
@@ -214,7 +204,7 @@ int main() {
 					smoothMoveToggleState = 0;
 				}
 				if(led && !increase && !decrease) {
-					do_linear_brightness(!led, led_slice_num, chan4);
+					do_linear_brightness(!led, led_slice_num, chan16);
 					state = 0;
 				}
 				break;
